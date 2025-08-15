ec8715fb6fa4998f078d280b741040187eb94b1c
Hoàng Thanh Trang <suakari08@gmail.com>
2025-08-15 11:54:28 +0700

    .

--- SwpInventory/src/java/dal/DBConnect.java
+++ SwpInventory/src/java/dal/DBConnect.java
@@ -9,7 +9,7 @@ public class DBConnect {
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         String url = "jdbc:sqlserver://localhost:1433;databaseName=Inventory;encrypt=true;trustServerCertificate=true";
         String username = "sa";
-        String password = "2910";
+        String password = "123";
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         return DriverManager.getConnection(url, username, password);
     }
